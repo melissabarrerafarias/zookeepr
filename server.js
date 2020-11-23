60a20e0fe18b1e5897a60688fdc339550d96d925
melissikiss <melissikiss@gmail.com>
2020-11-23 14:40:50 -0800

    added get animal by id function

--- server.js
+++ server.js
@@ -54,7 +54,11 @@ app.get('/api/animals', (req, res) => {
 
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
+    if (result) {
       res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
 
